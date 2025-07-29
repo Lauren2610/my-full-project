eea991aedce22d4a4b3a847774961fd15406b449
Lauren2610 <laurenshinthant@gmail.com>
2025-07-29 13:21:14 +0630

    dk

--- backend/python_project/views.py
+++ backend/python_project/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 
 def myapp(request):
     return render(request,'python_project/main.html')
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST','PUT','DELETE'])
 def getData(request):
     if request.method == 'GET':
         return Response(database.pokedex)
@@ -32,6 +32,33 @@ def getData(request):
                 }
 
             return Response(response_data, status=status.HTTP_201_CREATED)
+    elif request.method == 'PUT':
+            data = request.data
+            number = data.get('number')
+            if not number or int(number) not in database.pokedex:
+              return Response({'Message',status=status.HTTP_400_BAD_REQUEST})
+
+            name = pokedex.name
+            type = pokedex.type
+
+
+
+            try:
+                parsed_number = int(number)
+                pokemon = database.pokedex.get(parsed_number)
+
+                if pokemon:
+                    response_data = {
+                        'pokemon': pokemon,
+                        'status': 'success'
+                    }
+                else:
+                    response_data = {
+                        'message': 'empty',
+                        'status': 'success'
+                    }
+
+                return Response(response_data, status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return Response({
